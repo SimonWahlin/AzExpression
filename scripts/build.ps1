2b1b686562f8792728f465fe29eba57bf2003638
Simon Wåhlin <simon.wahlin@advania.com>
2022-10-27 23:11:43 +0200

    Add version parameter to build

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,3 +1,6 @@
+param(
+    $Version = [string]::Empty
+)
 # To build the module, make sure you have the modules "ModuleBuilder" and "platyPS" installed.
 
 Import-Module 'ModuleBuilder' -ErrorAction 'Stop'
@@ -6,12 +9,6 @@ Import-Module 'platyPS' -ErrorAction 'Stop'
 Push-Location -Path "$PSScriptRoot/../src"
 $null = New-ExternalHelp -Path '../docs/en-US' -OutputPath './en-US'
 
-try {
-    $Version = gitversion /showvariable LegacySemVerPadded
-}
-catch {
-    $Version = [string]::Empty
-}
 Build-Module -SemVer $Version
 Pop-Location
 
